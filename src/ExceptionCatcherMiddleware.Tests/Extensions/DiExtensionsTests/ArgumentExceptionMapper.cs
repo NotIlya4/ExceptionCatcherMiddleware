d54e5ef4ab94db2168745a6107cd601dfb055ee0
NotIlya <secretman44444@gmail.com>
2023-02-27 00:17:22 +0300

    Add DiExtensions tests

---
+++ src/ExceptionCatcherMiddleware.Tests/Extensions/DiExtensionsTests/ArgumentExceptionMapper.cs
@@ -0,0 +1,18 @@
+﻿using ExceptionCatcherMiddleware.Mappers.CreatingCustomMappers;
+
+namespace ExceptionCatcherMiddleware.UnitTests.Extensions.DiExtensions;
+
+public class ArgumentExceptionMapper : IExceptionMapper<ArgumentException>
+{
+    public BadResponse Map(ArgumentException exception)
+    {
+        return new BadResponse()
+        {
+            StatusCode = 400,
+            ExceptionDto = new
+            {
+                A = "a"
+            }
+        };
+    }
+}
